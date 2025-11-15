018cda5d1be1050c82fc3c27673acdf23ca01ae5
FeltrinLM <lorenzofmendes49@gmail.com>
2025-11-15 01:25:08 -0300

    pop up do login iniciado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/component';
+//temporario:
+import { AuthCardComponent } from './shared/components/auth-card/auth-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AuthCardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/shared/components/auth-card/auth-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-card', // Este será o nome da tag
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-card-component.html',
+  styleUrl: './auth-card-component.css'
+})
+export class AuthCardComponent {
+
+}
