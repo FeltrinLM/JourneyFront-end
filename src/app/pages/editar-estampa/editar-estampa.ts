7858a540b45d51d5ed4caae66caa8760ec3b8bc8
FeltrinLM <lorenzofmendes49@gmail.com>
2025-11-18 19:20:57 -0300

    visual do login feito

--- src/app/pages/editar-estampa/editar-estampa.ts
+++ src/app/pages/editar-estampa/editar-estampa.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 // 1. IMPORTA O BOTÃO DE VOLTAR
 import { BackButtonComponent } from '../../shared/components/back-button/back-button.component';
+import {AuthCardComponent} from '../../shared/components/auth-card/auth-card.component';
 
 @Component({
   standalone: true,
   selector: 'app-editar-estampa',
-    imports: [
-      CommonModule,
-      BackButtonComponent
-    ],
+  imports: [
+    CommonModule,
+    BackButtonComponent,
+    AuthCardComponent
+  ],
   templateUrl: './editar-estampa.html',
   styleUrl: './editar-estampa.css'
 })
